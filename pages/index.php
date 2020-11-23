cf5b6739b8b974f1bf73c4bbbe6235e18a94f4eb
Christopher Pitt <cgpitt@gmail.com>
2020-11-23 20:52:32 +0200

    Add cache example

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use Next\Cache;
 use Next\Http\Request;
 use Next\Http\Response;
 use Next\Session;
@@ -9,7 +10,7 @@ return function (Request $request, Response $response, array $params = []) {
     $session = app(Session::class);
     $session->set('count', (int) $session->get('count', 0) + 1);
 
-    $posts = Post::all();
+    $posts = app(Cache::class)->remember('posts', fn() => Post::all(), 5);
     $items = '<li>' . $posts->map(fn($post) => $post->title)->join('</li><li>') . '</li>';
     $count = $session->get('count', 0);
 
